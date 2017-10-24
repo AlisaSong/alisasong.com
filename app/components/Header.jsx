ee802cfe904c61837e85c98210ef98ec7868fba9
Alisa Song <alisasong@gmail.com>
2017-10-24 11:21:19 -0700

    Add header css file

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from '../index.css';
+import styles from './header.css';
 
 export default class Contact extends React.Component {
     constructor(props) {
