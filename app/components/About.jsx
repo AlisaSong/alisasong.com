21d59dd94877927a5a0e13e75b68168d1ad3df4c
Alisa Song <alisasong@gmail.com>
2017-10-25 23:41:48 -0700

    Add randomized circle and diamond decoration generator

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+﻿import React from 'react';
 
 import Header from './Header'
 
@@ -9,7 +9,23 @@ export default class About extends React.Component {
         super(props);
 
         this.state = {
+            circlesDiamondsLong: this.getCirclesDiamonds(300),
+            circlesDiamondsShort: this.getCirclesDiamonds(50)
         };
+    }
+
+    getCirclesDiamonds(size) {
+        let result = '';
+
+        for (let i = 0; i < size; i++) {
+            if (Math.floor(Math.random() * 2) == 0) {
+                result += '◆ ';
+            } else {
+                result += '● ';
+            }
+        }
+
+        return result;
     }
 
     render() {
@@ -17,11 +33,15 @@ export default class About extends React.Component {
             <section>
                 <Header />
                 <div className={styles.aboutSection}>
-                <h1>ABOUT ME</h1>
+                    <div className={styles.headerWrapper}>
+                        <div className={styles.circlesDiamondsShort}>{this.state.circlesDiamondsShort}</div>
+                        <div className={styles.aboutTitle}>ABOUT ME</div>
+                        <div className={styles.circlesDiamondsLong}>{this.state.circlesDiamondsLong}</div>
+                    </div>
 
-                <p className={styles.aboutContent}>
-                    Hi there! I am a front-end developer who builds high quality, responsive websites and apps. 
-                    I enjoy problem-solving with code and love learning new technologies that challenge my creativity and coding capabilities. 
+                    <p className={styles.aboutContent}>
+                        Hi there! I am a front-end developer who builds high quality, responsive websites and apps.
+                    I enjoy problem-solving with code and love learning new technologies that challenge my creativity and coding capabilities.
                     While you're here, take a look around and feel free to reach out if you'd like to start a conversation!
                     -Alisa</p>
                 </div>
