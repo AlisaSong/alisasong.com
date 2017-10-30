1019bde2fb97ef09177ed3e3660bceb7fc57120c
Alisa Song <alisasong@gmail.com>
2017-10-29 20:28:21 -0700

    Remove circle animation from About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,34 +9,8 @@ export default class About extends React.Component {
         super(props);
 
         this.state = {
-            circlesDiamondsLong: this.getCirclesDiamonds(300),
-            circlesDiamondsShort: this.getCirclesDiamonds(50),
             skills: ['JavaScript', 'React', 'Node.js', 'HTML', 'CSS', 'C#']
         };
-    }
-
-    getCirclesDiamonds(size) {
-        let result = [];
-
-        for (let i = 0; i < size; i++) {
-            if (this.getRandom(0, 1) === 0) {
-                result.push({
-                    cycle: styles['circlesDiamonds' + this.getRandom(1, 3)],
-                    display: '◆ '
-                });
-            } else {
-                result.push({
-                    cycle: styles['circlesDiamonds' + this.getRandom(1, 3)],
-                    display: '● '
-                });
-            }
-        }
-
-        return result;
-    }
-
-    getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     render() {
@@ -45,23 +19,7 @@ export default class About extends React.Component {
                 <Header />
                 <div className={styles.aboutSection}>
                     <div className={styles.aboutTitleWrapper}>
-                        <div className={styles.circlesDiamondsShort}>
-                            {this.state.circlesDiamondsShort.map((shape, index) =>
-                                <span className={shape.cycle}
-                                    key={index}>
-                                    {shape.display}
-                                </span>
-                            )}
-                        </div>
                         <div className={styles.aboutTitle}>ABOUT ME</div>
-                        <div className={styles.circlesDiamondsLong}>
-                            {this.state.circlesDiamondsLong.map((shape, index) =>
-                                <span className={shape.cycle}
-                                    key={index}>
-                                    {shape.display}
-                                </span>
-                            )}
-                        </div>
                     </div>
                     <p className={styles.aboutContent}>
                         Hi there! I am a front-end developer who builds high quality, responsive websites and apps.
@@ -70,23 +28,7 @@ export default class About extends React.Component {
                         -Alisa
                     </p>
                     <div className={styles.aboutTitleWrapper}>
-                        <div className={styles.circlesDiamondsShort}>
-                            {this.state.circlesDiamondsShort.map((shape, index) =>
-                                <span className={shape.cycle}
-                                    key={index}>
-                                    {shape.display}
-                                </span>
-                            )}
-                        </div>
                         <div className={styles.aboutTitle}>SKILLS</div>
-                        <div className={styles.circlesDiamondsLong}>
-                            {this.state.circlesDiamondsLong.map((shape, index) =>
-                                <span className={shape.cycle}
-                                    key={index}>
-                                    {shape.display}
-                                </span>
-                            )}
-                        </div>
                     </div>
                     <ul className={styles.skills}>
                         {this.state.skills.map((skill, index) =>
