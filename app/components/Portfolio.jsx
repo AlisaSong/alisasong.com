4c80af8ac9276ea0883375fff9029db45af33b0a
Alisa Song <alisasong@gmail.com>
2017-10-29 21:56:59 -0700

    Fix alignment of sections and images in Portfolio page, update sizing of elements

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -21,34 +21,36 @@ export default class Portfolio extends React.Component {
                 <article>
                     <div className={styles.portfolioWrapper}>
                         <h1 className={styles.portfolioTitle}>PORTFOLIO</h1>
-                        <div className={styles.projectSection}>
-                            <div className={styles.projectContentWrapper}>
+                        <div className={styles.projectContentWrapper}>
+                            <div className={styles.projectContent}>
                                 <h2 className={styles.projectTitle}>Happy Tummy Grill</h2>
-                                <div className={styles.projectDescription}>
-                                    <h3 className={styles.projectDescriptionTitle}>The Project</h3>
-                                    <p className={styles.projectDescriptionText}>
-                                        This client needed a website for their business to provide information about their menu and hours to their current customers
+                                <h3 className={styles.projectDescriptionTitle}>The Project</h3>
+                                <p className={styles.projectDescriptionText}>
+                                    This client needed a website for their business to provide information about their menu and hours to their current customers
                                     and draw in potential customers from the area. I built a clean, easy-to-read website that
                                     showcases their menu items and provides answers to basic questions about their restaurant.
                                     The website uses Google Analytics and since its launch, website traffic
                                     regularly spikes before lunch and dinner times.
                                 </p>
-                                    <h3 className={styles.projectDescriptionTitle}>Skills Used</h3>
-                                    <ul className={styles.projectDescriptionText}>
-                                        <li className={styles.projectBullet}>Web Development with JavaScript, HTML5, CSS3</li>
-                                        <li className={styles.projectBullet}>Telemetry with Google Analytics</li>
-                                        <li className={styles.projectBullet}>Web Copywriting</li>
-                                    </ul>
-                                    <div className={styles.projectButtons}>
-                                        <button className={styles.projectButton} type="button">Take a Look</button>
-                                        <button className={styles.projectButton} type="button">GitHub</button>
-                                    </div>
+                                <h3 className={styles.projectDescriptionTitle}>Skills Used</h3>
+                                <ul className={styles.projectDescriptionText}>
+                                    <li className={styles.projectBullet}>Web Development with JavaScript, HTML5, CSS3</li>
+                                    <li className={styles.projectBullet}>Telemetry with Google Analytics</li>
+                                    <li className={styles.projectBullet}>Web Copywriting</li>
+                                </ul>
+                                <div className={styles.projectButtons}>
+                                    <button className={styles.projectButton} type="button">Take a Look</button>
+                                    <button className={styles.projectButton} type="button">GitHub</button>
                                 </div>
                             </div>
-                            <div className={styles.projectMobileDisplay}>
+                        </div>
+                        <div className={styles.projectMobileDisplay}>
+                            <div className={styles.projectPhoneImageWrapper}>
                                 <img className={styles.projectPhoneImage} src={projectPhoneImage} />
                             </div>
-                            <div className={styles.projectDesktopDisplay}>
+                        </div>
+                        <div className={styles.projectDesktopDisplay}>
+                            <div className={styles.projectDesktopImageWrapper}>
                                 <img className={styles.projectDesktopImage} src={projectDesktopImage} />
                             </div>
                         </div>
