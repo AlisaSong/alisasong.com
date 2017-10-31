3a019dade7b7a694f92b770335a116db707d6868
Alisa Song <alisasong@gmail.com>
2017-10-30 22:49:42 -0700

    Add text color change animation to About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,6 +2,7 @@
 
 import Header from './Header';
 import Home from './Home';
+import WordAnimator from './WordAnimator';
 
 import globalStyles from '../styles/index.css';
 import styles from '../styles/about.css';
@@ -11,8 +12,17 @@ export default class About extends React.Component {
         super(props);
 
         this.state = {
-            skills: ['JavaScript', 'React', 'Node.js', 'HTML', 'CSS', 'C#']
+            delay: 1000,
+            languages: ['JavaScript', 'HTML', 'CSS', 'C#'],
+            frameworks: ['React'],
+            tools: ['Node.js', 'Git', 'GitHub', 'Visual Studio', 'Chrome DevTools']
         };
+    }
+
+    getDelay(word) {
+        let delay = this.state.delay;
+        this.state.delay += 50 * word.length;
+        return delay;
     }
 
     render() {
@@ -25,15 +35,26 @@ export default class About extends React.Component {
                         <div className={styles.aboutSection}>
                             <h1 className={styles.aboutTitle}>ABOUT ME</h1>
                             <p className={styles.aboutContent}>
-                                Hi there! I'm Alisa, a front-end developer based in Kirkland.
-                                I love building beautiful, high-quality websites and apps, and am passionate about learning new technologies to grow my coding expertise.
+                                Hi there! I'm <WordAnimator delay={this.getDelay('Alisa')} word="Alisa" />,
+                                a <WordAnimator delay={this.getDelay('front-end developer')} word="front-end developer" /> based in Kirkland.
+                                I love building beautiful, <WordAnimator delay={this.getDelay('high quality websites')} word="high quality websites" /> and
+                                apps, and
+                                am <WordAnimator delay={this.getDelay('passionate')} word="passionate" /> about <WordAnimator delay={this.getDelay('learning new technologies')} word="learning new technologies" /> to
+                                grow my <WordAnimator delay={this.getDelay('coding expertise')} word="coding expertise" />.
                                 In my free time, I enjoy practicing yoga, playing the flute, walking my dog and playing video games.
-                                While you're here, take a look around and feel free reach out if you'd like to start a conversation!
+                                While you're here, take a look around and feel free <WordAnimator delay={this.getDelay('reach out')} word="reach out" /> if
+                                you'd like to start a <WordAnimator delay={this.getDelay('conversation')} word="conversation" />!
                             </p>
                             <h1 className={styles.aboutTitle}>MY SKILLSET</h1>
                             <ul className={styles.skills}>
-                                {this.state.skills.map((skill, index) =>
-                                    <li className={styles.skill} key={index}>{skill}</li>
+                                {this.state.languages.map((language, index) =>
+                                    <li className={styles.language} key={index}>{language}</li>
+                                )}
+                                {this.state.frameworks.map((framework, index) =>
+                                    <li className={styles.framework} key={index}>{framework}</li>
+                                )}
+                                {this.state.tools.map((tool, index) =>
+                                    <li className={styles.tool} key={index}>{tool}</li>
                                 )}
                             </ul>
                         </div>
---
+++ app/components/WordAnimator.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import styles from '../styles/word-animator.css';
+
+export default class WordAnimator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            delay: props.delay,
+            letters: this.getLetters(props.word)
+        };
+    }
+
+    getDelay() {
+        this.state.delay += 50;
+        return { 'animation-delay': this.state.delay + 'ms' };
+    }
+
+    getLetters(word) {
+        let list = [];
+        for (let i = 0; i < word.length; i++) {
+            list.push(word[i]);
+        }
+        return list;
+    }
+
+    render() {
+        return (
+            <span>
+                {this.state.letters.map((letter, index) =>
+                    <span className={styles.pinkText}
+                        key={index}
+                        style={this.getDelay()}>
+                        {letter}
+                    </span>
+                )}
+            </span>
+        );
+    }
+}
