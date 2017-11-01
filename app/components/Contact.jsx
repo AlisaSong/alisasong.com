d921e03b6f0820afd48a1f7c42a725ccd0d12bce
Alisa Song <alisasong@gmail.com>
2017-10-31 23:47:12 -0700

    Build basic layout of contact form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './Header'
 
-import styels from '../styles/contact.css';
+import styles from '../styles/contact.css';
 import globalStyles from '../styles/index.css';
 
 export default class Contact extends React.Component {
@@ -17,9 +17,34 @@ export default class Contact extends React.Component {
         return (
             <section>
                 <Header />
-                <h1>LET'S COLLABORATE</h1>
-
-            </section>
+                <article>
+                    <div className={styles.contactWrapper}>
+                        <h1 className={styles.title}>LET'S COLLABORATE</h1>
+                        <div className={styles.contactContentWrapper}>
+                            <p className={styles.description}>Interested in working together? Fill out the form below & I promise to get back to you within 48 hrs.</p>
+                            <div className={styles.contactFormWrapper}>
+                                <label className={styles.label}>
+                                    <span>Name*</span>
+                                    <input type="text" />
+                                </label>
+                                <label className={styles.label}>
+                                    <span>Company*</span>
+                                    <input type="text" />
+                                </label>
+                                <label className={styles.label}>
+                                    <span>Email*</span>
+                                    <input type="text" />
+                                </label>
+                                <label className={styles.label}>
+                                    <span>Message*</span>
+                                    <input type="text" />
+                                </label>
+                                <button type="button">Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </article >
+            </section >
         );
     }
 }
